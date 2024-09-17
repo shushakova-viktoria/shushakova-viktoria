f2c582e8e4681537c5ac4e9c88552f9a820efd9e
shushakova-viktoria <shushakova-va@yandex.ru>
2024-09-17 16:07:17 +0600

    Add files via upload

---
+++ Lab1WithCorrections.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //1 задача
+
+        /*int a;
+        int b;
+        Console.WriteLine("Введите число a");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число b");
+        b = Convert.ToInt32(Console.ReadLine());
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine($"Теперь число a = {a}");
+        Console.WriteLine($"Теперь число b = {b}");/*
+
+        //2 задача 
+
+        /*int a, b, minim;
+        Console.WriteLine("Введите a");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите b");
+        b = Convert.ToInt32(Console.ReadLine());
+        minim = ((a + b) - Math.Abs((a - b))) / 2;
+        Console.WriteLine($"Минимальное число = {minim}");*/
+
+        //3 задача
+
+        /*int n, l, k, m, s;
+        Console.WriteLine("Введите количество грядок");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите ширину грядок");
+        l = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите расстояние от колодца до 1-ой грядки");
+        k = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите высоту грядок");
+        m = Convert.ToInt32(Console.ReadLine());
+        s = 2*n*(k+m+l)+((n - 1)*n)*l;
+        Console.WriteLine($"Исходное расстояние = {s}");*/
+
+
+
+    }
+}
